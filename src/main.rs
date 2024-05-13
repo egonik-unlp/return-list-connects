2acbf5e64c56ecefb8fc9c10a8f53f1e67122c55
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2024-05-13 14:56:10 -0300

    pulido

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
-use std::{os::linux::raw::stat, sync::Arc};
-
+use std::sync::Arc;
 use axum::{
-    extract::{ConnectInfo, State}, routing::get, Router
+    extract::{ConnectInfo, State},
+    routing::get,
+    Router,
 };
-use tokio::sync::Mutex;
-use std::net::SocketAddr;
 use axum_macros::debug_handler;
+use std::net::SocketAddr;
+use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
     let state: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
     let app = Router::new()
-    .route("/log", get(log))
-    .route("/clear", get(clear))
-    .route("/", get(root))
-    .with_state(state);
+        .route("/log", get(log))
+        .route("/clear", get(clear))
+        .route("/", get(root))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:2000").await.unwrap();
-    axum::serve(listener, app.into_make_service_with_connect_info::<SocketAddr>()).await.unwrap()
-
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap()
 }
 
-
-async fn root(State(state): State<Arc<Mutex<Vec<String>>>>) -> String  {
+async fn root(State(state): State<Arc<Mutex<Vec<String>>>>) -> String {
     let inner = state.lock().await;
     format!("{:#?}", inner)
-
 }
 
 async fn clear(State(state): State<Arc<Mutex<Vec<String>>>>) -> String {
@@ -34,9 +37,11 @@ async fn clear(State(state): State<Arc<Mutex<Vec<String>>>>) -> String {
     format!("{:#?}", inner)
 }
 
-
 #[debug_handler]
-async fn log(State(state): State<Arc<Mutex<Vec<String>>>>, ConnectInfo(connect_addr) : ConnectInfo<SocketAddr>) -> String {
+async fn log(
+    State(state): State<Arc<Mutex<Vec<String>>>>,
+    ConnectInfo(connect_addr): ConnectInfo<SocketAddr>,
+) -> String {
     let stri = format!("{:?}", connect_addr);
     let mut inner = state.lock().await;
     (*inner).push(stri.clone());
